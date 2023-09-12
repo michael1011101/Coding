154954d4e84ae87366a674b976218f9d1370c73a
Zhaohui(Michael) Wang <michael1011101@users.noreply.github.com>
2023-09-12 13:43:15 +0800

    Add solutions for database problems

---
+++ leetcode/Database/1965-Employees-With-Missing-Information.sql
@@ -0,0 +1,81 @@
+# Write your MySQL query statement below
+
+select
+  employee_id 
+from (
+  select e.employee_id from Employees as e left join salaries as s on e.employee_id=s.employee_id where s.employee_id is null
+  union
+  select s.employee_id from Employees as e right join salaries as s on e.employee_id=s.employee_id where e.employee_id is null
+) answer
+order by employee_id
+
+/*
+# 1965. Employees With Missing Information
+
+Easy
+
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+*/
